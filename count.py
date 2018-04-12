81d0cc60f241e11f39a476a444bcae3df743cf12
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-04-13 00:15:58 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,16 @@
+a  = int(input("Enter the number:"))
+
+if a < 10:
+  print "1"
+  
+if a > 9 and a < 100:
+  print "2"
+  
+if a > 99 and a < 1000:
+  print "3"
+  
+if a > 999 and a < 10000:
+  print "4"
+  
+if a > 9999 and a < 100000:
+  print "5"
