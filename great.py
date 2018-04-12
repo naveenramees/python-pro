242f780d3dad04b1ee0c79c9abdc8b7b00e7fd5b
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-04-12 23:45:50 +0530

    Create great.py

---
+++ great.py
@@ -0,0 +1,14 @@
+for i in range(0,3):
+  if i == 0:
+    a = int(input("\nEnter the num1:"))
+  if i == 1:
+    b = int(input("\nEnter the num2:"))
+  if i == 2:
+    c = int(input("\nEnter the num3:"))
+    
+    
+def ashwintaj(n1,n2,n3):
+  print max(n1,n2,n3)
+  
+
+ashwintaj(a,b,c)  
