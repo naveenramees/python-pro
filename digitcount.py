0c0fcefb72fa6f871fee901eb8540ce46f070475
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-04-13 00:23:11 +0530

    Create digitcount.py

---
+++ digitcount.py
@@ -0,0 +1,18 @@
+box  = int(input("Enter the number:"))
+
+if box < 10:
+	print "1"
+  
+if box > 9 and box < 100:
+	print "2"
+  
+if box > 99 and box < 1000:
+	print "3"
+  
+if box > 999 and box < 10000:
+	print "4"
+  
+if box > 9999 and box < 100000:
+	print "5"
+if box > 9999 and box < 1000000:
+	print "6"
