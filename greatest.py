155fc4d951815a4bec43abad0480f99e75014c66
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-04-12 23:48:15 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,14 @@
+for z in range(0,3):
+  if z == 0:
+    a = int(input("\nEnter the num1:"))
+  if z == 1:
+    b = int(input("\nEnter the num2:"))
+  if z == 2:
+    c = int(input("\nEnter the num3:"))
+    
+    
+def ashwintaj(n1,n2,n3):
+  print max(n1,n2,n3)
+  
+
+ashwintaj(a,b,c)  
