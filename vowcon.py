5ab8b81246f24cea07d91c4458a409cdb5b6fea2
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-04-13 18:29:22 +0530

    Create vowcon.py

---
+++ vowcon.py
@@ -0,0 +1,8 @@
+ero = raw_input("Enter a character: ")
+
+if(ero=='A' or ero=='a' or ero=='O' or ero =='o' or ero=='U'
+ or ero=='u' or ero=='E' or ero=='e' or ero=='I' or ero=='i'):
+    print "Vowel"
+else:
+    print "Consonant"
+     
