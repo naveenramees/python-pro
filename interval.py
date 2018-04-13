b844ed333a3c0771fb6db698dc5c92e8f4ea21c3
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-04-13 18:56:47 +0530

    Create interval.py

---
+++ interval.py
@@ -0,0 +1,14 @@
+for i in range(0,2):
+  if i == 0:
+    m = int(input("Enter the firnum:"))
+    
+  if i == 1:
+    n = int(input("Enter the secnum:"))
+    
+m = m + 1
+
+for z in range(m,n):
+  if z%2 == 0:
+    print z
+    
+    
