fb865dfbead86b2a47015c6f74875bd32599929a
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-04-13 18:25:13 +0530

    Create checkvowcon.py

---
+++ checkvowcon.py
@@ -0,0 +1,8 @@
+ero = raw_input("Enter a character: ")
+
+if(ero=='A' or ero=='a' or ero=='E' or ero =='e' or ero=='I'
+ or ero=='i' or ero=='O' or ero=='o' or ero=='U' or ero=='u'):
+    print "Vowel"
+else:
+    print "Consonant"
+     
