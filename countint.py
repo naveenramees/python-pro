4e02b15343a29f6b19d3aaa294a74447610bb027
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-07 00:52:38 +0530

    Create countint.py

---
+++ countint.py
@@ -0,0 +1,20 @@
+v = int(input())
+
+if v > 0 and v < 10:
+  print 1
+  
+  
+if v >= 10 and v < 100:
+  print 2
+  
+if v >=100 and v <1000:
+  print 3
+  
+if v >=1000 and v<10000:
+  print 4
+  
+if v>=10000 and v<100000:
+  print 5
+  
+if v>=100000 and v<1000000:
+  print 6
