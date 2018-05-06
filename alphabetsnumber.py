01e867630ffa76265532531897c673a2e05a3c88
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-07 00:37:50 +0530

    Create alphabetsnumber.py

---
+++ alphabetsnumber.py
@@ -0,0 +1,87 @@
+
+g = int(input())
+
+if g == 1:
+  print "A"
+  
+if g == 2:
+  print "B"
+if g == 3:
+  print "C"
+if g == 4:
+  print "D"
+  
+  
+if g == 5:
+  print "E"
+  
+if g == 6:
+  print "F"
+  
+  
+if g == 7:
+  print "G"
+  
+if g == 8:
+  print "H"
+  
+if g == 9:
+  print "I"
+  
+if g == 10:
+  print "j"
+  
+if g == 11:
+  print "K"
+  
+if g == 12:
+  print "L"
+  
+if g == 13:
+  print "M"
+  
+if g == 14:
+  print "N"
+  
+if g == 15:
+  print "O"
+  
+if g == 16:
+  print "P"
+  
+if g == 17:
+  print "Q"
+  
+if g == 18:
+  print "R"
+  
+if g == 19:
+  print "S"
+  
+if g == 20:
+  print "T"
+  
+  
+if g == 21:
+  print "U"
+  
+if g ==22:
+  print "V"
+  
+if g == 23:
+  print "W"
+  
+  
+if g == 24:
+  print "X"
+ 
+if g == 25:
+  print "Y"
+  
+
+
+if g == 26:
+  print "Z"
+    
+  
+    
