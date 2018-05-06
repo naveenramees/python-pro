f24e38a043cdf639be3d246737853d47199fae4b
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-06 16:33:43 +0530

    Create numericcount.py

---
+++ numericcount.py
@@ -0,0 +1,19 @@
+ash = int(input())
+
+if ash >= 0 and ash < 10:
+  print "1"
+  
+if ash >=10 and ash < 100:
+  print "2"
+  
+if ash >=100 and ash < 1000:
+  print "3"
+  
+if ash >=1000 and ash < 10000:
+  print "4"
+  
+if ash >=10000 and ash < 100000:
+  print "5"
+  
+if ash >=100000 and ash < 1000000:
+  print "6"
