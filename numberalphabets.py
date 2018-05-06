3bcc90f5726cec921b5a1a0793483e7747e78a80
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-07 00:40:51 +0530

    Create numberalphabets.py

---
+++ numberalphabets.py
@@ -0,0 +1,89 @@
+r = int(input())
+
+if r == 1:
+  print "A"
+  
+if r == 2:
+  print "B"
+if g == 3:
+  print "C"
+if r == 4:
+  print "D"
+  
+  
+if r == 5:
+  print "E"
+  
+if r == 6:
+  print "F"
+  
+  
+if r == 7:
+  print "G"
+  
+if r == 8:
+  print "H"
+  
+if r == 9:
+  print "I"
+  
+if r == 10:
+  print "j"
+  
+if r == 11:
+  print "K"
+  
+if r == 12:
+  print "L"
+  
+if r == 13:
+  print "M"
+  
+if r == 14:
+  print "N"
+  
+if r == 15:
+  print "O"
+  
+if r == 16:
+  print "P"
+  
+if r == 17:
+  print "Q"
+  
+if r == 18:
+  print "R"
+  
+if r == 19:
+  print "S"
+  
+if r == 20:
+  print "T"
+  
+  
+if r == 21:
+  print "U"
+  
+if r ==22:
+  print "V"
+  
+if r == 23:
+  print "W"
+  
+  
+if r == 24:
+  print "X"
+ 
+if r == 25:
+  print "Y"
+  
+
+
+if r == 26:
+  print "Z"
+    
+  
+    
+  
+  
+  
