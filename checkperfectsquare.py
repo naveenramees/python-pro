ec3a219a748a31405adffd3feeb3a9d72b95259f
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-06 22:29:25 +0530

    Create checkperfectsquare.py

---
+++ checkperfectsquare.py
@@ -0,0 +1,9 @@
+g,n = map(int,raw_input().split())
+
+k = g*n
+
+if k == 1 or k == 4 or k == 9 or k == 16 or k == 25 or k == 36 or k == 49 or k == 64 or k == 81 or k == 100:
+  print "yes"
+  
+else:
+  print "no"
