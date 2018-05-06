3174feb1c785788f420f8d694ef733dd3b489cae
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-06 20:47:55 +0530

    Create greatpowof2.py

---
+++ greatpowof2.py
@@ -0,0 +1,25 @@
+w = int(input())
+
+if w < 2:
+  print "2"
+
+if w >=2 and w < 4:
+  print "4"
+  
+if w >=4 and w < 8:
+  print "8"
+  
+if w >=8 and w < 16:
+  print "16"
+ 
+if w >=16 and w < 32:
+  print "32"
+  
+if w >=32 and w < 64:
+  print "64"
+  
+if w >=64 and w < 128:
+  print "128"
+  
+if w >=128 and w < 256:
+  print "256"
