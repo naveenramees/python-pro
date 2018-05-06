049d2b91b9ef804c30bf1f4e72430483cd71b177
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-06 20:08:59 +0530

    Create notoword.py

---
+++ notoword.py
@@ -0,0 +1,32 @@
+raj = int(input())
+
+if raj == 1:
+  print "One"
+  
+elif raj == 2:
+  print "Two"
+  
+elif raj == 3:
+  print "Three"
+  
+elif raj == 4:
+  print "Four"
+  
+elif raj == 5:
+  print "Five"
+  
+elif raj == 6:
+  print "Six"
+  
+elif raj ==7:
+  print "seven"
+  
+elif raj ==8:
+  print "Eight"
+  
+elif raj ==9:
+  print "Nine"
+
+elif raj ==10:
+  print "Ten"
+  
