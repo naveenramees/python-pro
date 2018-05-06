0213c5f2e69897912908016f646ec7f9526562eb
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-06 22:39:05 +0530

    Create printfactors.py

---
+++ printfactors.py
@@ -0,0 +1,52 @@
+
+i = int(input())
+
+
+if i%1 == 0:
+  print 1,
+
+if i%2 == 0:
+  print 2,
+ 
+if i%3 == 0:
+  print 3,
+  
+if i%4 == 0:
+  print 4,
+  
+if i%5 == 0:
+  print 5,
+  
+if i%6 == 0:
+  print 6,
+  
+if i%7 == 0:
+  print 7,
+  
+if i%8 == 0:
+  print 8,
+  
+if i%9 == 0:
+  print 9,
+  
+if i%10 == 0:
+  print 10,
+  
+if i%11 == 0:
+  print 11,
+  
+if i%12 == 0:
+  print 12,
+  
+if i%13 == 0:
+  print 13,
+  
+if i%14 == 0:
+  print 14,
+  
+if i%15 == 0:
+  print 15,
+  
+if i%16 == 0:
+  print 16,
+  
