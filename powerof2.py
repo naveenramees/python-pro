996a64f60d2185f5679e84550a34cd3c06e3afba
naveenramees <37336736+naveenramees@users.noreply.github.com>
2018-05-06 18:04:22 +0530

    Create powerof2.py

---
+++ powerof2.py
@@ -0,0 +1,27 @@
+no = int(input())
+
+
+if no == 1:
+  print "yes"
+
+elif no == 2:
+  print "yes"
+
+elif no == 4:
+  print "yes"
+
+elif no == 8:
+  print "yes"
+
+elif no == 16:
+  print "yes"
+
+elif no == 32:
+  print "yes"
+
+elif no == 64:
+  print "yes"
+  
+else:
+  print "no"
+
